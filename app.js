ba505c2e25bfbdcd49a4d0cefb0ed0ba24f1332f
Ojiefoh <ehiojiefoh@leisdstudent.ws>
2022-04-06 08:54:32 -0500

    changed onclick function

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-function onClick () {
-    
+function transform () {
+    document.getElementById("heading").style.fontSize = "40px";
+    document.getElementById("heading").innerHTML = "Behold!";
+    document.getElementById("heading").style.backgroundColor = "orange";
+    document.getElementById("heading").style.padding = "20px";
+    console.log("It worked!");
 };
 
