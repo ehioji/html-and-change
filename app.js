cc62dbff8c7e78ccd444704c67adc975e2944f87
Ojiefoh <ehiojiefoh@leisdstudent.ws>
2022-04-08 08:36:29 -0500

    added user input and corresponding function

--- app.js
+++ app.js
@@ -5,12 +5,26 @@ function transform () {
     geez.style.backgroundColor = "orange";
     geez.style.padding = "20px";
     randomNumber();
+    userInputStuff();
 };
 
 function randomNumber () {
     deez = document.getElementById("smol_heading");
-    deez.style.fontSize = "40px";
     deez.innerHTML = Math.floor(Math.random() * 10);
+    deez.style.fontSize = "40px";
     deez.style.backgroundColor = "orange";
     deez.style.padding = "20px";
+    deez.style.textAlign = "center"
 }
+
+function userInputStuff () {
+    let x = document.getElementById("userInput").value;
+
+    let y = document.getElementById("userNumber");
+
+    y.innerHTML = x;
+    y.style.color = "green";
+    y.style.backgroundColor = "blue";
+    y.style.padding = "30px";
+    y.style.textAlign = "center";
+};
